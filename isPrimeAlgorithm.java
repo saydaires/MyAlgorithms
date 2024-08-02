904063e15e87301ecf9b3a16e6bd4db39d93ad78
Sayd Aires <159132545+saydaires@users.noreply.github.com>
2024-08-02 10:12:51 -0300

    Update isPrimeAlgorithm.java
    Upgrades that increase robustness of the algorithm.

--- isPrimeAlgorithm.java
+++ isPrimeAlgorithm.java
@@ -1,7 +1,11 @@
 public static boolean isPrime(int number)
 {
+    if(number < 2)
+        return false; // any number less than 2 is not prime;
     if(number == 2)
         return true; // '2' is the only even number that is prime;
+    if(number % 2 == 0)
+        return false; // this verify eliminated any number bigger than 2 that is even;
 
     int divisiblesCount = 1; // including the number '1'
     for(int index = 2; index <= number; index++)
